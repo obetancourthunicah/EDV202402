991750783d116970ebfb8094cba2636ab6be8a20
obetancourthunicah <obetancourthunicah@gmail.com>
2024-06-05 18:49:40 -0600

    Libreria de ClinicaMedica

---
+++ ClinicaMedicaLib/Medico.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ClinicaMedicaLib
+{
+    public class Medico: Personas 
+    {
+        private string _especialidad;
+
+        public string Especialidad { get => _especialidad; set => _especialidad = value; }
+
+        public Medico() {
+            _especialidad = "";
+        }
+
+        public Medico(
+            string id,
+            string nombre,
+            string correo,
+            string telefono,
+            string especialidad
+            ) {
+            Id = id;
+            Nombre = nombre;
+            Correo = correo;
+            Telefono = telefono;
+            Especialidad = especialidad;
+        }
+    }
+}
---
+++ ClinicaMedicaLib/Personas.cs
@@ -0,0 +1,35 @@
+﻿namespace ClinicaMedicaLib
+{
+    public class Personas
+    {
+        private string _id;
+        private string _nombre;
+        private string _correo;
+        private string _telefono;
+
+        public string Id { get => _id; set => _id = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Correo { get => _correo; set => _correo = value; }
+        public string Telefono { get => _telefono; set => _telefono = value; }
+
+        public Personas() { 
+            _id ="";
+            _nombre = "";
+            _correo = "";
+            _telefono = "";
+        }
+
+        public Personas(
+            string id,
+            string nombre,
+            string correo,
+            string telefono
+            )
+        {
+            _id = id;
+            _nombre = nombre;
+            _correo = correo;
+            _telefono = telefono;
+        }
+    }
+}
